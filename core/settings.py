f0f181a4091bb998c3c0f22c129f12ab36e2a722
RiqueAlvess <henrique.alves.siqueira@hotmail.com>
2025-04-07 08:29:37 -0300

    Updt: Empresa Ativa vinculo automatico

--- core/settings.py
+++ core/settings.py
@@ -75,13 +75,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+from decouple import config
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.parse(config('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
